235a81e72085b1d1ae39dc16008eac164f79cdfc
pratiksaha <Pratik_Saha@epam.com>
2020-04-15 22:40:49 +0530

    initial commit

---
+++ homework2/src/app/status.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'status'
+})
+export class StatusPipe implements PipeTransform {
+
+  transform(value: string, statusnow : boolean): any {
+    var statusElement = document.getElementById("status");
+    if (statusnow === true) {
+      status = "Deactivated";
+      statusElement.setAttribute("target", "NotActive");
+    } else if (statusnow === false) {
+      status = "Active";
+      statusElement.setAttribute("target", "Active");
+    }
+    else{
+      status="unknown";
+    }
+    return status;
+  }
+
+}
